3d922473e70b8aee8bae362ac22fd3f368017f96
Muhammad Shahzaib <shahzaib823786@gmail.com>
2026-07-10 18:43:40 +0500

    update files

--- Day8/mobilemenu.js
+++ Day8/mobilemenu.js
@@ -1,5 +1,5 @@
-const menuBtn = document.querySelector("#menuBtn");            //query selectore is used for id and class it may catch the id by #id and can catch class as well like .class//
-const navLinks = document.querySelector("#navLinks");
+const menuBtn = document.querySelector("#menubtn");            //query selectore is used for id and class it may catch the id by #id and can catch class as well like .class//
+const navLinks = document.querySelector("#navlinks");
 menuBtn.addEventListener("click", () => {
 // add "open" if missing, remove it if present
 navLinks.classList.toggle("open");
